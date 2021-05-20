6d1548eb0e41ee8d38d4faaa89608399fc817591
Josef Andersson <37870813+janderssonse@users.noreply.github.com>
2021-05-20 14:51:10 +0200

    gradle 7.x. cleanup readme. minor cleanups. java 1.8 (#1)
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,18 @@
 plugins {
-  //  id("java-gradle-plugin")
     kotlin("jvm") version "1.4.31"
- //   id("maven-publish")
-    id("se.ascp.gradle.gradle-versions-filter") version "0.1.4" 
-    id("org.owasp.dependencycheck") version "6.1.2"
+    id("se.ascp.gradle.gradle-versions-filter") version "0.1.6"
+    id("org.owasp.dependencycheck") version "6.1.6"
     id("pl.allegro.tech.build.axion-release") version "1.13.2"
- //   id("com.gradle.plugin-publish") version "0.13.0"
-    id("se.svt.oss.gradle-yapp-publisher-plugin") version "0.1.10"
+    id("io.gitlab.arturbosch.detekt") version "1.17.0"
+    id("se.svt.oss.gradle-yapp-publisher-plugin") version "0.1.11"
 }
-
-//order matters for axion, configure this before project.version
-//scmVersion {
-//    tag.apply {
- //       prefix = "v"
-  //      versionSeparator = ""
-  //  }
-//}
 
 group = "se.ascp.gradle"
 project.version = scmVersion.version
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 
@@ -37,34 +27,14 @@ tasks {
     }
 }
 
-/*pluginBundle {
-    website = "https://github.com/jandersson-svt/gradle-versions-filter-plugin"
-    vcsUrl = "https://github.com/jandersson-svt/gradle-versions-filter-plugin.git"
-    tags = listOf("versions", "filter")
-}
-
-gradlePlugin {
-    plugins {
-        create("simplePlugin") {
-            id = "se.ascp.gradle.gradle-versions-filter"
-            displayName = "Gradle Versions Filter Plugin"
-            description = "Extension of Gradle Versions Plugin (discovering dependency updates) with opinionated defaults."
-            implementationClass = "se.ascp.gradle.GradleVersionsFilterPlugin"
-        }
-    }
-}
-*/
 dependencies {
     implementation("com.github.ben-manes:gradle-versions-plugin:[0.38.0,1.0.0)")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
 }
-
-
 
 tasks.named<Wrapper>("wrapper") {
     distributionType = Wrapper.DistributionType.ALL
-    gradleVersion = "6.8.3"
+    gradleVersion = "7.0.2"
 }
-
