861aee48b4157f41ce6e0432081971dd902fc45b
Josef Andersson <josef.andersson@gmail.com>
2022-04-25 11:00:21 +0200

    chore: upgrade dependencies
    Signed-off-by: Josef Andersson <josef.andersson@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,21 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.4.31" //stick to the supported Gradle plugin version https://docs.gradle.org/current/userguide/compatibility.html
+    kotlin("jvm") version "1.6.0" //stick to the supported Gradle plugin version https://docs.gradle.org/current/userguide/compatibility.html
     id("java-gradle-plugin")
-    id("se.ascp.gradle.gradle-versions-filter") version "0.1.9"
-    id("org.owasp.dependencycheck") version "6.2.2"
-    id("pl.allegro.tech.build.axion-release") version "1.13.3"
-    id("io.gitlab.arturbosch.detekt") version "1.17.1"
-    id("se.svt.oss.gradle-yapp-publisher-plugin") version "0.1.13"
+    id("se.ascp.gradle.gradle-versions-filter") version "0.1.10"
+    id("org.owasp.dependencycheck") version "7.1.0.1"
+    id("pl.allegro.tech.build.axion-release") version "1.13.6"
+    id("io.gitlab.arturbosch.detekt") version "1.20.0"
+    id("se.svt.oss.gradle-yapp-publisher-plugin") version "0.1.15"
 }
 
 group = "se.ascp.gradle"
-project.version = scmVersion.version
+project.version = "1.0.0-SNAPSHOT"//scmVersion.version
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
-
-
 repositories {
     gradlePluginPortal()
     mavenCentral()
@@ -32,11 +30,11 @@ tasks {
 dependencies {
     implementation("com.github.ben-manes:gradle-versions-plugin:[0.39.0,1.0.0)")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.named<Wrapper>("wrapper") {
     distributionType = Wrapper.DistributionType.ALL
-    gradleVersion = "7.1.1"
+    gradleVersion = "7.4.2"
 }
