4b9d396bc00623fdd53f613294eeb5187e2cc34c
Josef Andersson <josef.andersson@gmail.com>
2022-05-19 09:30:58 +0200

    refactor: follow gradle plugin structure conv
    Signed-off-by: Josef Andersson <josef.andersson@gmail.com>

--- gradle-versions-filter/build.gradle.kts
+++ gradle-versions-filter/build.gradle.kts
@@ -2,11 +2,11 @@
 plugins {
     kotlin("jvm") version "1.6.0" // stick to the supported Gradle plugin version https://docs.gradle.org/current/userguide/compatibility.html
     id("java-gradle-plugin")
-    id("se.ascp.gradle.gradle-versions-filter") version "0.1.13+"
+    id("se.ascp.gradle.gradle-versions-filter") version "0.1.14"
     id("org.owasp.dependencycheck") version "7.1.0.1"
     id("pl.allegro.tech.build.axion-release") version "1.13.6"
     id("io.gitlab.arturbosch.detekt") version "1.20.0"
-    id("se.svt.oss.gradle-yapp-publisher-plugin") version "0.1.16"
+    id("se.svt.oss.gradle-yapp-publisher") version "0.1.18"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
 
@@ -17,11 +17,6 @@ kotlin {
     jvmToolchain {
         (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(11))
     }
-}
-
-repositories {
-    gradlePluginPortal()
-    mavenCentral()
 }
 
 tasks {
@@ -35,9 +30,4 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-}
-
-tasks.named<Wrapper>("wrapper") {
-    distributionType = Wrapper.DistributionType.ALL
-    gradleVersion = "7.4.2"
 }
